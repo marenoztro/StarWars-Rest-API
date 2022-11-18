64892d68191936c1bff4e0cb63178cebd70d6fa5
marenoztro <marlon.marotto@gmail.com>
2022-11-18 22:27:43 +0000

    todo ok

--- src/main.py
+++ src/main.py
@@ -30,16 +30,96 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
+
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
+
+#     return jsonify(response_body), 200
+
+
+
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets.query.all()
+    results = list(map(lambda item: item.serialize(), planets))
+    print(results)
+    # print(planets)
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "RESPUESTA :D"
     }
 
     return jsonify(response_body), 200
+
+@app.route('/planets/<int:planets_id>', methods=['GET'])
+def get_one_planet(planets_id):
+    planet = Planets.query.filter_by(id=planets_id).first()
+    print(planet)
+    
+    response_body = planet.serialize()
+
+    return jsonify(response_body), 200
+
+
+
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    results = list(map(lambda item: item.serialize(), people))
+    print(results)
+    
+    response_body = {
+        "msg": "Hello, this is your GET /people response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_one_people(people_id):
+    people = People.query.filter_by(id=people_id).first()
+    print(people.serialize())
+    
+    response_body = people.serialize()
+
+    return jsonify(response_body), 200
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    usuarios = Usuario.query.all()
+    results = list(map(lambda item: item.serialize(), usuarios))
+    print(results)
+    
+    response_body = {
+        "msg": "Hello, this is your GET /users response "
+    }
+
+    return jsonify(results), 200
+
+@app.route('/users/<int:usuario_id>', methods=['GET'])
+def get_users_one():
+    usuario = Usuario.query.filter_by(id=usuario_id).first()
+    result = usuario.serialize()
+    print(result)
+    
+    response_body = {
+        "msg": " BIEN FUNCIONÓ :D"
+    }
+
+    return jsonify(response_body), 200
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+    # results = list(map(lambda item: item.serialize(), people))
--- src/models.py
+++ src/models.py
@@ -17,3 +17,74 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    favorito = db.relationship('Favorites', backref='user', lazy=True)
+    
+    def __repr__(self):
+        return '<Usuario %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    eye_color = db.Column(db.String(80), unique=False, nullable=False)
+    favorito = db.relationship('Favorites', backref='people', lazy=True)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye_color
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    weather = db.Column(db.String(80), unique=False, nullable=False)
+    favorito = db.relationship('Favorites', backref='planets', lazy=True)
+    
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "weather": self.weather
+        }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'),
+        nullable=False)
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'),
+        nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'),
+        nullable=False)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "people_id": self.people_id,
+            "planets_id": self.planets_id,
+            "user_id": self.user_id
+        }
